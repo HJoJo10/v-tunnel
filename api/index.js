700780baded3dffd18c75ea03c1d9afc8a05326e
HJoJo10 <Hamidofgosale2@outlook.com>
2026-04-30 01:35:49 -0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,12 +4,14 @@ export const config = {
 
 export default async function handler(req) {
   const url = new URL(req.url);
-  const targetUrl = 'https://Faz.jojeyenaz.ir:2095' + url.pathname + url.search;
+  const targetUrl = 'http://Faz.jojeyenaz.ir:2095' + url.pathname + url.search;
 
-  return fetch(targetUrl, {
+  const res = await fetch(targetUrl, {
     method: req.method,
     headers: req.headers,
     body: req.body,
     redirect: 'manual'
   });
+
+  return res;
 }
